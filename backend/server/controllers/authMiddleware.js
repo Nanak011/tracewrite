d1f95b36ece9c8710ed524e05c64828ff216f9fb
Nanak011 <gurunanakadhikari1@gmail.com>
2026-07-01 15:02:41 +0545

    P8.2 Editor with full collaboration and color highlights.

--- backend/server/controllers/authMiddleware.js
+++ backend/server/controllers/authMiddleware.js
@@ -2,6 +2,7 @@ function requireAuth(req, res, next) {
   if (!req.session.user) {
     return res.status(401).json({ error: "Unauthorized" });
   }
+  req.user = req.session.user;
   return next();
 }
 
